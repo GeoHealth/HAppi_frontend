efddcf2581ad9cb144fe07ea65a461fb3d6f3443
Kagelmacher <alexandre.hauet@gmail.com>
2017-03-29 12:23:46 +0200

    fixe https://travis-ci.org/GeoHealth/HAppi_frontend/builds/216289143

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './home';
 import { ReportComponent } from './report';
 import { NoContentComponent } from './no-content';
 
-
 export const ROUTES: Routes = [
   { path: '',      component: HomeComponent },
   { path: 'home',  component: HomeComponent },
