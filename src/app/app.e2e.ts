a638d8c295fda2afa36bc683c36056ddece26680
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-03-29 10:49:31 +0200

    fix test...Promise can not be assigned to string...

--- src/app/app.e2e.ts
+++ src/app/app.e2e.ts
@@ -6,28 +6,36 @@ describe('App', () => {
     browser.get('/');
   });
 
-  it('should have a title', () => {
-    let subject = browser.getTitle();
-    let result  = 'Angular2 Webpack Starter by @gdi2290 from @AngularClass';
-    expect(subject).toEqual(result);
+  it('should have a title', (done) => {
+    browser.getTitle().then((title) => {
+      let result = 'Angular2 Webpack Starter by @gdi2290 from @AngularClass';
+      expect(title).toEqual(result);
+      done();
+    });
   });
 
-  it('should have header', () => {
-    let subject = element(by.css('h1')).isPresent();
-    let result  = true;
-    expect(subject).toEqual(result);
+  it('should have header', (done) => {
+    element(by.css('h1')).isPresent().then((present) => {
+      let result = true;
+      expect(present).toEqual(result);
+      done();
+    });
   });
 
-  it('should have <home>', () => {
-    let subject = element(by.css('app home')).isPresent();
-    let result  = true;
-    expect(subject).toEqual(result);
+  it('should have <home>', (done) => {
+    element(by.css('app home')).isPresent().then((present) => {
+      let result = true;
+      expect(present).toEqual(result);
+      done();
+    });
   });
 
-  it('should have buttons', () => {
-    let subject = element(by.css('button')).getText();
-    let result  = 'Submit Value';
-    expect(subject).toEqual(result);
+  it('should have buttons', (done) => {
+    element(by.css('button')).getText().then((text) => {
+      let result = 'Submit Value';
+      expect(text).toEqual(result);
+      done();
+    });
   });
 
 });
--- src/app/home/home.e2e.ts
+++ src/app/home/home.e2e.ts
@@ -7,16 +7,20 @@ describe('App', () => {
     browser.get('/#/home');
   });
 
-  it('should have a title', () => {
-    let subject = browser.getTitle();
-    let result  = 'Angular2 Webpack Starter by @gdi2290 from @AngularClass';
-    expect(subject).toEqual(result);
+  it('should have a title', (done) => {
+    browser.getTitle().then((title) => {
+      let result = 'Angular2 Webpack Starter by @gdi2290 from @AngularClass';
+      expect(title).toEqual(result);
+      done();
+    });
   });
 
-  it('should have `your content here` x-large', () => {
-    let subject = element(by.css('[x-large]')).getText();
-    let result  = 'Your Content Here';
-    expect(subject).toEqual(result);
+  it('should have `your content here` x-large', (done) => {
+    element(by.css('[x-large]')).getText().then((text) => {
+      let result = 'Your Content Here';
+      expect(text).toEqual(result);
+      done();
+    });
   });
 
 });
