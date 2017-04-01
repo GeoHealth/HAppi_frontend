5ee142c2a95ed1237c469ea8c9e6780ed8d74c95
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-04-01 18:00:39 +0200

    remove unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,6 @@ import { APP_RESOLVER_PROVIDERS } from './app.resolver';
 import { AppState, InternalStateType } from './app.service';
 import { HomeComponent } from './home';
 import { ReportComponent } from './report';
-import { NoContentComponent } from './no-content';
 import { XLargeDirective } from './home/x-large';
 
 import '../styles/styles.scss';
@@ -57,7 +56,6 @@ type StoreType = {
     AppComponent,
     ReportComponent,
     HomeComponent,
-    NoContentComponent,
     XLargeDirective
   ],
   imports: [ // import Angular's modules
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home';
 import { ReportComponent } from './report';
-import { NoContentComponent } from './no-content';
 
 export const ROUTES: Routes = [
   { path: '',      component: HomeComponent },
   { path: 'home',  component: HomeComponent },
   { path: 'report', component: ReportComponent },
-  { path: '**',    component: NoContentComponent },
 ];
