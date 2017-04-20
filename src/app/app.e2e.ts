54b7df86f60f9f2da4fb69f46a0ad69a79a76e90
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-04-21 01:08:06 +0200

    remove test that I forgot in previous commit

--- src/app/app.e2e.ts
+++ src/app/app.e2e.ts
@@ -6,14 +6,6 @@ describe('App', () => {
     browser.get('/');
   });
 
-  it('should have a title', (done) => {
-    browser.getTitle().then((title) => {
-      let result = 'Angular2 Webpack Starter by @gdi2290 from @AngularClass';
-      expect(title).toEqual(result);
-      done();
-    });
-  });
-
   it('should have header', (done) => {
     element(by.css('h1')).isPresent().then((present) => {
       let result = true;
@@ -26,14 +18,6 @@ describe('App', () => {
     element(by.css('app home')).isPresent().then((present) => {
       let result = true;
       expect(present).toEqual(result);
-      done();
-    });
-  });
-
-  it('should have buttons', (done) => {
-    element(by.css('button')).getText().then((text) => {
-      let result = 'Submit Value';
-      expect(text).toEqual(result);
       done();
     });
   });
